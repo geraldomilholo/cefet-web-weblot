ac74f0e3d6a0b9297af74e193b79234587ccde92
Geraldo <geraldomilholo@MacBook-Air-de-Geraldo.local>
2017-05-22 22:57:39 -0300

    Examples

--- apis/canvas/script/main.js
+++ apis/canvas/script/main.js
@@ -1,6 +1,7 @@
 var sun = new Image();
 var moon = new Image();
 var earth = new Image();
+
 function init() {
   sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
   moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
@@ -9,39 +10,62 @@ function init() {
 }
 
 function draw() {
-  var ctx = document.getElementById('canvas').getContext('2d');
-
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-  ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-  ctx.save();
-  ctx.translate(150, 150);
-
-  // Earth
-  var time = new Date();
-  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 50, 24); // Shadow
-  ctx.drawImage(earth, -12, -12);
-
-  // Moon
-  ctx.save();
-  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-  ctx.translate(0, 28.5);
-  ctx.drawImage(moon, -3.5, -3.5);
-  ctx.restore();
-
-  ctx.restore();
   
-  ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-  ctx.stroke();
+  //-- Pega o elemento e informa que é 2D
+  var canvas = document.getElementById('canvas').getContext('2d');
+
+  canvas.globalCompositeOperation = 'destination-over';
+
+  //-- Limpa a área de desenho
+  canvas.clearRect(0, 0, 300, 300); 
+
+  canvas.fillStyle = 'rgba(0, 0, 0, 0.4)';
+  canvas.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+  canvas.save();
+  canvas.translate(150, 150);
+
+  //-- Terra
+  var time = new Date();
+  canvas.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  canvas.translate(105, 0);
+  canvas.fillRect(0, -12, 50, 24);
+  canvas.drawImage(earth, -12, -12);
+
+  //-- Lua
+  canvas.save();
+  canvas.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+  canvas.translate(0, 28.5);
+  canvas.drawImage(moon, -3.5, -3.5);
+  canvas.restore();
+
+  canvas.restore();
+  
+  //-- Criando a Órbita da Terra
+  canvas.beginPath();
+  canvas.arc(150, 150, 105, 0, Math.PI * 2, false);
+  canvas.stroke();
  
-  ctx.drawImage(sun, 0, 0, 300, 300);
+  //-- Sol
+  canvas.drawImage(sun, 0, 0, 300, 300);
 
   window.requestAnimationFrame(draw);
+
+  //-- Graph
+
+  var ctx = document.getElementById('graph').getContext('2d');
+  var img = new Image();
+  img.onload = function() {
+    ctx.drawImage(img, 60, 0);
+    ctx.beginPath();
+    ctx.moveTo(30, 96);
+    ctx.lineTo(70, 66);
+    ctx.lineTo(103, 76);
+    ctx.lineTo(170, 15);
+    ctx.stroke();
+  };
+  
+  img.src = 'https://mdn.mozillademos.org/files/5395/backdrop.png';
+
 }
 
 init();
